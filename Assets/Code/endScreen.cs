b8fd809cd83ed0147f419df1f3c36e0749471d5f
Alexander Glover <alexander.e.glover@gmail.com>
2017-10-01 17:04:08 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/GrievanceGames/GameJam2017""
    This reverts commit 95cb2dc0bc2bd7747710a0bd0aecd5ddb91466a9.

---
+++ Assets/Code/endScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class endScreen : MonoBehaviour {
+
+	public Text TextObject;
+	public int hiddenButtons;
+
+	// Use this for initialization
+	void Start () {
+		hiddenButtons = StaticVariableStorage.instance.GetHiddenFound();
+		TextObject.text = "The Garden is not impressed with your morality. You found " + hiddenButtons + " alternative options in the world that is not binary. The Garden will let you leave. For now. Press ESC to exit.";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
